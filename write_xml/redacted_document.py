fe57d4405ca3f241e40b5f31bf59496e06bc559f
devopcasting <devopcasting@gmail.com>
2024-04-08 12:29:44 +0530

    refactor

--- write_xml/redacted_document.py
+++ write_xml/redacted_document.py
@@ -1,141 +1,96 @@
 import os
 import re
 import xml.etree.ElementTree as ET
+from ocrr_log_mgmt.ocrr_log import OCRREngineLogging
 
 class WriteRedactedDocumentXML:
     def __init__(self, result_path: str, original_document_name: str, result_data: list ) -> None:
         self.xml_path = result_path
         self.xml_file_name = original_document_name
         self.result_data = result_data
+        self._setup_logging()
 
-    def writexml(self):
-        """Set XML file path"""
-        xml_file_path = os.path.join(self.xml_path, self.xml_file_name.split('.')[0]+'.xml')
-        if os.path.exists(xml_file_path):
-            os.remove(xml_file_path)
-        
-        """
-            Prepare Data 
-            - Frame-ID
-            - Document ID
-        """
-        pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
-        frame_str = self.xml_file_name.split('.')[0].split('-')[0]
-        matched_str = re.match(pattern, self.xml_file_name)
-        frame_id = 0
-
-        if matched_str:
-            frame_id = int(frame_str.split('F')[0]) - 1
-            document_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
-        else:
-            doc_id_num = re.split('_', self.xml_file_name)[0]
-            document_id = doc_id_num[:-1]
-        
-        """Prepare XML Data"""
-        xml_data = []
-        count_index = 1
-        coordinates_list = []
-        for i in self.result_data:
-            k_list = i['coordinates']
-            for j in k_list:
-                if len(j) != 0:
-                    coordinates_list.append(j)
-
-        for i in coordinates_list:
-            x1, y1, x2, y2 = i
-            xml_data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{frame_id},{document_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
-            x1, y1, x2, y2 = [0, 0, 0, 0]
-            count_index = count_index + 1
-        
-        """Create the root element"""
-        root = ET.Element("DataBase")
-
-        """Add count element"""
-        count = ET.SubElement(root, "Count")
-        count.text = f'{len(xml_data)}'
-
-        """Create the DatabaseRedactions element"""
-        database_redactions = ET.SubElement(root, "DatabaseRedactions")
-
-        """Create DatabaseRedaction elements in a loop"""
-        for i, item in enumerate(xml_data, start=1):
-            database_redaction = ET.SubElement(database_redactions, "DatabaseRedaction", ID=str(i))
-            database_redaction.text = item
-        
-        """Create an ElementTree object"""
-        tree = ET.ElementTree(root)
-
-        """Write the XML to a file"""
-        tree.write(xml_file_path , encoding="utf-8", xml_declaration=True)
+    def _setup_logging(self):
+        log_config = OCRREngineLogging()
+        self.logger = log_config.configure_logger()
     
-    def write_redacted_data_xml(self):
-        """Set XML file path"""
-        redacted_text_xml_name = self._rename_xml_file(self.xml_file_name)
-        xml_file_path = os.path.join(self.xml_path, redacted_text_xml_name)
-        if os.path.exists(xml_file_path):
-            os.remove(xml_file_path)
-        """
-            Prepare Data 
-            - Frame-ID
-            - Document ID
-        """
-        pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
-        frame_str = self.xml_file_name.split('.')[0].split('-')[0]
-        matched_str = re.match(pattern, self.xml_file_name)
-        frame_id = 0
+    def _parse_document_info(self):
+        try:
+            pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
+            frame_str = self.xml_file_name.split('.')[0].split('-')[0]
+            matched_str = re.match(pattern, self.xml_file_name)
 
-        if matched_str:
-            frame_id = int(frame_str.split('F')[0]) - 1
-            document_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
-        else:
-            doc_id_num = re.split('_', self.xml_file_name)[0]
-            document_id = doc_id_num[:-1]
-        
-        """Prepare XML data"""
+            if matched_str:
+                frame_id = int(frame_str.split('F')[0]) - 1
+                document_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
+            else:
+                doc_id_num = re.split('_', self.xml_file_name)[0]
+                frame_id = 0
+                document_id = doc_id_num[:-1]
+            return frame_id, document_id
+        except Exception as e:
+            self.logger.error(f"| Parsing redacted document info: {e}")
+
+    def _prepare_coordinates_xml_data(self, frame_id, document_id):
         xml_data = []
         count_index = 1
+        coordinates_list = [j for i in self.result_data for j in i['coordinates'] if len(j) != 0]
+
+        for x1, y1, x2, y2 in coordinates_list:
+            xml_data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{frame_id},{document_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
+            count_index += 1
+
+        return xml_data
+
+    def _prepare_text_xml_data(self, frame_id, document_id):
+        xml_data = []
         for i in self.result_data:
-            title_text = list(i.keys())[0]
-            value_text = i[title_text]
+            title_text, value_text = list(i.items())[0]
             xml_data.append(f'"Title": "{title_text}", "FrameID": "{frame_id}", "DocID": "{document_id}", "Value": "{value_text}"')
-            count_index = count_index + 1
+        return xml_data
 
-        """Create the root element"""
+    def _write_xml(self, xml_data, element_name):
         root = ET.Element("DataBase")
-
-        """Add count element"""
         count = ET.SubElement(root, "Count")
-        count.text = f'{len(xml_data)}'
+        count.text = str(len(xml_data))
+        database_element = ET.SubElement(root, element_name)
 
-        """Create the DatabaseRedactions element"""
-        database_redactions = ET.SubElement(root, "indexvalues")
-
-        """Create DatabaseRedaction elements in a loop"""
         for i, item in enumerate(xml_data, start=1):
-            database_redaction = ET.SubElement(database_redactions, "indexvalue", ID=str(i))
-            database_redaction.text = item
-        
-        """Create an ElementTree object"""
-        tree = ET.ElementTree(root)
+            sub_element = ET.SubElement(database_element, element_name[:-1], ID=str(i))
+            sub_element.text = str(item)
 
-        """Write the XML to a file"""
+        xml_file_path = os.path.join(self.xml_path, self._rename_xml_file(self.xml_file_name, element_name))
+        tree = ET.ElementTree(root)
         tree.write(xml_file_path , encoding="utf-8", xml_declaration=True)
 
-    def _rename_xml_file(self, filename: str) -> str:
+    def write_xml(self):
+        try:
+            self.logger.info(f"| Writing Redacted Coordinates in XML: {self.xml_file_name}")
+            frame_id, document_id = self._parse_document_info()
+            coordinates_xml_data = self._prepare_coordinates_xml_data(frame_id, document_id)
+            self._write_xml(coordinates_xml_data, "DatabaseRedactions")
+            return True
+        except Exception as e:
+            self.logger.error(f"| Writing Redacted Coordinates XML: {e}")
+            return False
+
+    def write_redacted_data_xml(self):
+        try:
+            self.logger.info(f"| Writing Redacted Text in XML: {self.xml_file_name}")
+            frame_id, document_id = self._parse_document_info()
+            text_xml_data = self._prepare_text_xml_data(frame_id, document_id)
+            self._write_xml(text_xml_data, "indexvalues")
+            return True
+        except Exception as e:
+            self.logger.error(f"| Writing Redacted Text XML: {e}")
+            return False
+
+    @staticmethod
+    def _rename_xml_file(filename: str, element_name: str) -> str:
         filename_list = filename.split('_', 1)
-        new_filename = f"{filename_list[0]}-RD_{filename_list[-1]}"
-        new_filename = new_filename.rsplit('.', 1)[0] + '.xml'
-        return new_filename
+        if element_name == "indexvalues":
+            new_filename = f"{filename_list[0]}-RD_{filename_list[-1]}"
+        else:
+            new_filename = filename
 
-
-
-
-
-        
-
-
-
-
-
-
-
+        return new_filename.rsplit('.', 1)[0] + '.xml'
--- write_xml/rejected_document.py
+++ write_xml/rejected_document.py
@@ -1,62 +1,71 @@
 import os
 import re
 import xml.etree.ElementTree as ET
+from ocrr_log_mgmt.ocrr_log import OCRREngineLogging
 
 class WriteRejectedDocumentXML:
     def __init__(self, redacted_path: str, original_document_name: str, ocr_obj_info: list) -> None:
         self.xml_path = redacted_path
         self.xml_file_name = original_document_name
-        self.content = ocr_obj_info 
+        self.content = ocr_obj_info
+        self._setup_logging()
 
-    def writexml(self) -> bool:
+    def _setup_logging(self):
+        log_config = OCRREngineLogging()
+        self.logger = log_config.configure_logger()
+    
+    def _extract_ids(self) -> tuple:
+        try:
+            # Extract frame ID and document ID from the file name
+            pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
+            frame_str = self.xml_file_name.split('.')[0].split('-')[0]
+            matched_str = re.match(pattern, self.xml_file_name)
+            if matched_str:
+                frame_id = int(frame_str.split('F')[0]) - 1
+                doc_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
+            else:
+                doc_id_num = re.split('_', self.xml_file_name)[0]
+                doc_id = doc_id_num[:-1]
+                frame_id = 0
+            return frame_id, doc_id
+        except Exception as e:
+            self.logger.error(f"| Parsing rejected document info: {e}")
 
-        # Set XML file path
-        xml_file_path = os.path.join(self.xml_path, self.xml_file_name.split('.')[0]+'.xml')
-        if os.path.exists(xml_file_path):
-            os.remove(xml_file_path)
-
-        # Prepare data
-        # Check if Frame-ID is available
-        pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
-        frame_str = self.xml_file_name.split('.')[0].split('-')[0]
-        matched_str = re.match(pattern, self.xml_file_name)
-        frame_id = 0
-        if matched_str:
-            frame_id = int(frame_str.split('F')[0]) - 1
-            # Get Document ID
-            doc_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
-        else:
-            # Get Document ID
-            doc_id_num = re.split('_', self.xml_file_name)[0]
-            doc_id = doc_id_num[:-1]
-
+    def _prepare_data(self) -> list:
+        # Prepare data for XML
         data = []
         count_index = 1
-        for i in self.content:
-            x1, y1, x2, y2 = i
-            #data.append(f'REJECTED,0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{frame_id},{doc_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
-            data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{frame_id},{doc_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
-            x1, y1, x2, y2 = [0, 0, 0, 0]
-            count_index = count_index + 1
-            
-        # Create the root element    
+        for coords in self.content:
+            x1, y1, x2, y2 = coords
+            data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{self.frame_id},{self.doc_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
+            count_index += 1
+        return data
+
+    def _create_xml_structure(self, data: list) -> ET.Element:
+        # Create XML structure
         root = ET.Element("DataBase")
-
-        # Add count element
         count = ET.SubElement(root, "Count")
-        count.text = f'{len(data)}'
-
-        # Create the DatabaseRedactions element
+        count.text = str(len(data))
         database_redactions = ET.SubElement(root, "DatabaseRedactions")
-
-        # Create DatabaseRedaction elements in a loop
         for i, item in enumerate(data, start=1):
             database_redaction = ET.SubElement(database_redactions, "DatabaseRedaction", ID=str(i))
             database_redaction.text = item
-        
-        # Create an ElementTree object
+        return root
+
+    def writexml(self) -> bool:
+        # Extract IDs
+        self.frame_id, self.doc_id = self._extract_ids()
+
+        # Prepare data
+        data = self._prepare_data()
+
+        # Create XML structure
+        root = self._create_xml_structure(data)
         tree = ET.ElementTree(root)
 
-        # Write the XML to a file or print it
-        tree.write(xml_file_path , encoding="utf-8", xml_declaration=True)
+        # Write XML to file
+        xml_file_path = os.path.join(self.xml_path, f"{self.xml_file_name.split('.')[0]}.xml")
+        if os.path.exists(xml_file_path):
+            os.remove(xml_file_path)
+        tree.write(xml_file_path, encoding="utf-8", xml_declaration=True)
         return True
