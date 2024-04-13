e029f6943e1f6ee4ec0eb37bb4c73fd80e4017bc
devopcasting <devopcasting@gmail.com>
2024-04-13 20:47:22 +0530

    improved logic

--- helper/passport_text_coordinates.py
+++ helper/passport_text_coordinates.py
@@ -2,16 +2,19 @@ import pytesseract
 import re
 
 class TextCoordinates:
-    def __init__(self, image_path) -> None:
+    def __init__(self, image_path, lang_type=None) -> None:
         self.image_path = image_path
-        
+        self.lang_type = lang_type
     
     # func: generate coordinates
     def generate_text_coordinates(self) -> list:
-        tesseract_config = r'--oem 3 --psm 11'
-        data = pytesseract.image_to_data(self.image_path, output_type=pytesseract.Output.DICT, config=tesseract_config)
+        if self.lang_type is None:
+            tesseract_config = r'--oem 3 --psm 11'
+            data = pytesseract.image_to_data(self.image_path, output_type=pytesseract.Output.DICT, config=tesseract_config)
+        elif self.lang_type == "default":
+            data = pytesseract.image_to_data(self.image_path, output_type=pytesseract.Output.DICT, lang="eng")
+
         special_characters = r'[!@#$%^&*()_\-+{}\[\]:;,.?~\\|]'
-    
         coordinates = []
 
         for i in range(len(data['text'])):
