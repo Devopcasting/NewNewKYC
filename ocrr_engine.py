3660d828e2e9cb9fa3b979f61af5f5d57725020c
devopcasting <devopcasting@gmail.com>
2024-04-26 19:46:20 +0530

    updated thread call

--- ocrr_engine.py
+++ ocrr_engine.py
@@ -2,6 +2,7 @@ import configparser
 import threading
 import queue
 import sys
+from concurrent.futures import ThreadPoolExecutor
 from config.mongodb_connection import EastablishMongoDBConnection
 from in_progress_status.filter_in_progress_status import FilterInProgressStatusDocuments
 from process_docs.start_processing_docs import ProcessDocuments
@@ -12,40 +13,42 @@ class OCRREngine:
         self.doc_upload_path = doc_upload_path
         self.workspace_path = workspace_path
         self.logger = OCRREngineLogging().configure_logger()
+        self.db_client = None
 
         try:
             self._initialize_mongodb_connection()
-            self._change_upload_status()
-            self.in_progress_status_q = queue.Queue()
+            if self.db_client:
+                self._change_upload_status()
+                self.in_progress_status_q = queue.Queue()
         except Exception as e:
             self.logger.error(f"| Failed to start OCRR Engine: {e}")
-            sys.exit(1)
+            
     
     def _initialize_mongodb_connection(self):
         self.logger.info("| Starting OCRR Engine")
         try:
-            db_client = EastablishMongoDBConnection().establish_connection()
-            if db_client is None:
+            self.db_client = EastablishMongoDBConnection().establish_connection()
+            if self.db_client is None:
                 self.logger.error("| Failed to establish connection to MongoDB")
                 self.logger.info("| Stopping OCRR Engine")
-                sys.exit(1)
+                
             
-            self.db_upload = db_client["upload"]
+            self.db_upload = self.db_client["upload"]
             self.db_filedetails = self.db_upload["fileDetails"]
-            db_name_list = db_client.list_database_names()
+            db_name_list = self.db_client.list_database_names()
             
             if "ocrrworkspace" not in db_name_list:
                 self.logger.info("| Creating ocrrworkspace database and ocrr collection")
-                db_client["ocrrworkspace"].create_collection("ocrr")
+                self.db_client["ocrrworkspace"].create_collection("ocrr")
             else:
                 self.logger.info("| Resetting ocrr collection")
-                db_client["ocrrworkspace"]["ocrr"].drop()
+                self.db_client["ocrrworkspace"]["ocrr"].drop()
 
             self.logger.info("| Connection established with MongoDB")
         except Exception as e:
             self.logger.error(f"| Error in initializing MongoDB: {e}")
             self.logger.info("| Stopping OCRR Engine")
-            db_client.close()
+            self.db_client.close()
             sys.exit(1)
         
     def _change_upload_status(self):
@@ -53,21 +56,32 @@ class OCRREngine:
         self.logger.info("| Changed status 'IN_QUEUE' to 'IN_PROGRESS'")
 
     def start_ocrr_engine(self):
-        try:
-            in_progress_status_doc_thread = threading.Thread(target=self._query_in_progress_status_doc)
-            process_documents_thread = threading.Thread(target=self._process_documents)
+        if self.db_client is None:
+            self.logger.error("| Failed to establish connection to MongoDB")
+        else:
+            try:
+                with ThreadPoolExecutor(max_workers=2) as executor:
+                    executor.submit(self._query_in_progress_status_doc)
+                    executor.submit(self._process_documents)
+            except Exception as e:
+                self.logger.error(f"| An error occurred during OCRR engine execution: {e}")
+                sys.exit(1)
 
-            """Start Threads"""
-            in_progress_status_doc_thread.start()
-            process_documents_thread.start()
+        # try:
+        #     in_progress_status_doc_thread = threading.Thread(target=self._query_in_progress_status_doc)
+        #     process_documents_thread = threading.Thread(target=self._process_documents)
 
-            """Join Threads"""
-            in_progress_status_doc_thread.join()
-            process_documents_thread.join()
+        #     """Start Threads"""
+        #     in_progress_status_doc_thread.start()
+        #     process_documents_thread.start()
 
-        except Exception as e:
-            self.logger.error(f"| An error occurred during OCRR engine execution: {e}")
-            sys.exit(1)
+        #     """Join Threads"""
+        #     in_progress_status_doc_thread.join()
+        #     process_documents_thread.join()
+
+        # except Exception as e:
+        #     self.logger.error(f"| An error occurred during OCRR engine execution: {e}")
+        #     sys.exit(1)
 
     def _query_in_progress_status_doc(self):
         filter_in_progress_status_doc = FilterInProgressStatusDocuments(self.doc_upload_path, self.in_progress_status_q)
